e7ae589c1a3428a0e965fc12ce1ca2c6b6241a62
ThibaultRic <mail@thibault-richard.com>
2016-04-17 23:44:46 +0200

    indent work

--- index.php
+++ index.php
@@ -21,20 +21,20 @@
       <div class="navbar-fixed">
         <nav>
           <div class="nav-wrapper teal">
-              <a href="index.php" class="brand-logo center">Sneaker Shop</a>
-              <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-              <ul class="left hide-on-med-and-down">
-                  <li><a href="#">produits</a></li>
-                  <li><a href="collapsible.html">Javascript</a></li>
-                  <li><a href="mobile.html">Mobile</a></li>
-              </ul>
-              <ul class="side-nav teal" id="mobile-demo">
-                  <li><a href="sass.html" class="white-text">Sass</a></li>
-                  <li><a href="badges.html" class="white-text">Components</a></li>
-                  <li><a href="collapsible.html" class="white-text">Javascript</a></li>
-                  <li><a href="mobile.html" class="white-text">Mobile</a></li>
-              </ul>
-              <a href="#"><i class="right material-icons">shopping_cart</i></a>
+            <a href="index.php" class="brand-logo center">Sneaker Shop</a>
+            <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+            <ul class="left hide-on-med-and-down">
+                <li><a href="#">produits</a></li>
+                <li><a href="collapsible.html">Javascript</a></li>
+                <li><a href="mobile.html">Mobile</a></li>
+            </ul>
+            <ul class="side-nav teal" id="mobile-demo">
+                <li><a href="sass.html" class="white-text">Sass</a></li>
+                <li><a href="badges.html" class="white-text">Components</a></li>
+                <li><a href="collapsible.html" class="white-text">Javascript</a></li>
+                <li><a href="mobile.html" class="white-text">Mobile</a></li>
+            </ul>
+            <a href="#"><i class="right material-icons">shopping_cart</i></a>
           </div>
         </nav>
       </div>
@@ -64,119 +64,116 @@
           <div class="row">
             <h2 class="header center teal-text">Découvrez les nouveaux modèles</h2>
           </div>
-          <div class="row">
-        <?php
-          mysql_connect("localhost", "root", "tIbO") or
-          die("Impossible de se connecter : " . mysql_error());
-          mysql_select_db("sneakerShop");
- 
-          $result = mysql_query("SELECT * FROM product ORDER BY created_at DESC LIMIT 6");
- 
-          while ($row = mysql_fetch_array($result, MYSQL_BOTH)) {
-            $name = $row["name"];
-            $subName = $row["subName"];
-            $productPic = $row["productPic"];
-            $price = $row["price"];
-            $description = $row["description"];
+        <div class="row">
+          <?php
+            mysql_connect("localhost", "root", "tIbO") or
+            die("Impossible de se connecter : " . mysql_error());
+            mysql_select_db("sneakerShop");
+            $result = mysql_query("SELECT * FROM product ORDER BY created_at DESC LIMIT 6");
+            while ($row = mysql_fetch_array($result, MYSQL_BOTH)) {
+              $name = $row["name"];
+              $subName = $row["subName"];
+              $productPic = $row["productPic"];
+              $price = $row["price"];
+              $description = $row["description"];
+          ?>
+              <div class=" col l4 m12 s12 card">
+                <div class="card-image waves-effect waves-block waves-light">
+                  <img class="activator responsive-img" src="src/img/products/<?php echo $productPic; ?>">
+                </div>
+                <div class="card-content">
+                  <span class="card-title activator grey-text text-darken-4"><?php echo $name; ?><i class="material-icons right">more_vert</i></span>
+
+                  <p><?php echo $subName; ?></p>
+                </div>
+
+                <div class="card-action">
+                  <a href="#" class="teal-text"><?php echo $price; ?>€</a>
+                  <a href="#" class="teal-text"><i class="material-icons right">add_shopping_cart</i></a>
+                </div>
+
+                <div class="card-reveal">
+                  <span class="card-title grey-text text-darken-4"><?php echo $name; ?><i class="material-icons right">close</i></span>
+                  <p>
+                    <?php echo $description;?>
+                  </p>
+                  <p><a href="#" class="teal-text">plus de détails</a></p>
+                </div>
+              </div>
+            <?php
+              }
+              mysql_free_result($result);
             ?>
+        </div>
+      </div>
+      <div class="section teal"></div>
+      <!-- End last Products -->
 
-            <div class=" col l4 m12 s12 card">
-              <div class="card-image waves-effect waves-block waves-light">
-                <img class="activator responsive-img" src="src/img/products/<?php echo $productPic; ?>">
+      <div class="parallax-container">
+        <div class="parallax"><img class="backgroundImg" src="src/img/sneakerVecto.jpeg"></div>
+      </div>
+
+      <!-- Contact Form -->
+      <div class="section container white">
+        <div class="row">
+          <h2 class="header center teal-text">Vous avez une question?</h2>
+          <p class="grey-text center">Posez-nous toutes vos questions et inscrivez vous à notre newsletter afin d'être le premier au courant des dernières sorties.</p>
+        </div>
+        <div class="row">
+          <form class="col s12">
+            <div class="row">
+              <div class="input-field col s6">
+                <input id="first_name" type="text" class="validate" required>
+                <label for="first_name">Prénom</label>
               </div>
-              <div class="card-content">
-                <span class="card-title activator grey-text text-darken-4"><?php echo $name; ?><i class="material-icons right">more_vert</i></span>
-
-                <p><?php echo $subName; ?></p>
-              </div>
-
-              <div class="card-action">
-                <a href="#" class="teal-text"><?php echo $price; ?>€</a>
-                <a href="#" class="teal-text"><i class="material-icons right">add_shopping_cart</i></a>
-              </div>
-
-              <div class="card-reveal">
-                <span class="card-title grey-text text-darken-4"><?php echo $name; ?><i class="material-icons right">close</i></span>
-                <p>
-                  <?php echo $description;?>
-                </p>
-                <p><a href="#" class="teal-text">plus de détails</a></p>
+              <div class="input-field col s6">
+                <input id="last_name" type="text" class="validate" required>
+                <label for="last_name">Nom</label>
               </div>
             </div>
-            <?php
-          }
-          mysql_free_result($result);
-          ?>
-
+            <div class="row">
+              <div class="input-field col s12">
+                <input id="email" type="email" class="validate" required>
+                <label for="email">Email</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <select required>
+                  <option value="" disabled selected>Choisissez la raison</option>
+                  <option value="1">Je cherche un modèle en particulier</option>
+                  <option value="2">Informations sur une commande</option>
+                  <option value="3">Disponibilité des produits</option>
+                  <option value="4">Autre raison</option>
+                </select>
+                <label>Raisons de la prise de contact</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <textarea id="textarea1" class="materialize-textarea" length="500" required></textarea>
+                <label for="textarea1">Votre message</label>
+              </div>
+            </div>
+            <p>
+              <input type="checkbox" id="test5" />
+              <label for="test5">S'inscrire à la newsletter</label>
+            </p>
+            <div class="row center">
+              <button class="btn-large teal waves-effect waves-light" type="submit" name="action">Envoyer
+                <i class="material-icons right">send</i>
+              </button>
+            </div>
+          </form>
         </div>
-
-        </div>
-                <div class="section teal"></div>
-
-        <!-- End last Products -->
-
-        <div class="parallax-container">
-          <div class="parallax"><img class="backgroundImg" src="src/img/sneakerVecto.jpeg"></div>
-        </div>
-        <div class="section container white">
-          <div class="row">
-            <h2 class="header center teal-text">Vous avez une question?</h2>
-            <p class="grey-text center">Posez-nous toutes vos questions et inscrivez vous à notre newsletter afin d'être le premier au courant des dernières sorties.</p>
-          </div>
-          <div class="row">
-            <form class="col s12">
-              <div class="row">
-                <div class="input-field col s6">
-                  <input id="first_name" type="text" class="validate" required>
-                  <label for="first_name">Prénom</label>
-                </div>
-                <div class="input-field col s6">
-                  <input id="last_name" type="text" class="validate" required>
-                  <label for="last_name">Nom</label>
-                </div>
-              </div>
-              <div class="row">
-                <div class="input-field col s12">
-                  <input id="email" type="email" class="validate" required>
-                  <label for="email">Email</label>
-                </div>
-              </div>
-              <div class="row">
-                <div class="input-field col s12">
-                  <select required>
-                    <option value="" disabled selected>Choisissez la raison</option>
-                    <option value="1">Je cherche un modèle en particulier</option>
-                    <option value="2">Informations sur une commande</option>
-                    <option value="3">Disponibilité des produits</option>
-                    <option value="4">Autre raison</option>
-                  </select>
-                  <label>Raisons de la prise de contact</label>
-                </div>
-              </div>
-              <div class="row">
-                <div class="input-field col s12">
-                  <textarea id="textarea1" class="materialize-textarea" length="500" required></textarea>
-                  <label for="textarea1">Votre message</label>
-                </div>
-              </div>
-              <p>
-                <input type="checkbox" id="test5" />
-                <label for="test5">S'inscrire à la newsletter</label>
-              </p>
-              <div class="row center">
-                <button class="btn-large teal waves-effect waves-light" type="submit" name="action">Envoyer
-                  <i class="material-icons right">send</i>
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
+      </div>
+      <!-- End Contact Form -->
 
       <!-- End Content -->
 
       <!-- Footer -->
       <footer class="page-footer teal">
-          <div class="container">
+        <div class="container">
           <div class="row">
             <div class="col l4 s12">
               <h5 class="white-text">Sneaker Shop</h5>
@@ -192,17 +189,16 @@
               </ul>
             </div>
           </div>
-          </div>
-          <div class="footer-copyright">
+        </div>
+        <div class="footer-copyright">
           <div class="container">
-          Thibault Production
-          <a class="grey-text text-lighten-4 right" href="#!">site map</a>
+            Thibault Production
+            <a class="grey-text text-lighten-4 right" href="#!">site map</a>
           </div>
-          </div>
+        </div>
       </footer>
       <!-- End Footer -->
-      </div>
-
+      
       <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
       <script type="text/javascript" src="js/materialize.min.js"></script>
