2c5b79b76d4bdb475996251a09b0c2a7d1fe4160
ThibaultRic <mail@thibault-richard.com>
2016-04-17 17:07:03 +0200

    ajout Card product

--- index.php
+++ index.php
@@ -15,74 +15,156 @@
     </head>
 
     <body>
+      <!-- Menu -->
       <div class="navbar-fixed">
-
-        <!-- Menu -->
         <nav>
-            <div class="nav-wrapper teal">
-                <a href="index.php" class="brand-logo center">Sneaker Shop</a>
-                <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-                <ul class="left hide-on-med-and-down">
-                    <li><a href="#">produits</a></li>
-                    <li><a href="collapsible.html">Javascript</a></li>
-                    <li><a href="mobile.html">Mobile</a></li>
-                </ul>
-                <ul class="side-nav teal" id="mobile-demo">
-                    <li><a href="sass.html" class="white-text">Sass</a></li>
-                    <li><a href="badges.html" class="white-text">Components</a></li>
-                    <li><a href="collapsible.html" class="white-text">Javascript</a></li>
-                    <li><a href="mobile.html" class="white-text">Mobile</a></li>
-                </ul>
-                <a href="#" data-activates="mobile-demo"><i class="right material-icons">shopping_cart</i>
-            </div>
+          <div class="nav-wrapper teal">
+              <a href="index.php" class="brand-logo center">Sneaker Shop</a>
+              <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+              <ul class="left hide-on-med-and-down">
+                  <li><a href="#">produits</a></li>
+                  <li><a href="collapsible.html">Javascript</a></li>
+                  <li><a href="mobile.html">Mobile</a></li>
+              </ul>
+              <ul class="side-nav teal" id="mobile-demo">
+                  <li><a href="sass.html" class="white-text">Sass</a></li>
+                  <li><a href="badges.html" class="white-text">Components</a></li>
+                  <li><a href="collapsible.html" class="white-text">Javascript</a></li>
+                  <li><a href="mobile.html" class="white-text">Mobile</a></li>
+              </ul>
+              <a href="#" ><i class="right material-icons">shopping_cart</i></a>
+          </div>
         </nav>
-        <!-- End Menu -->
+      </div>
+      <!-- End Menu -->
 
-        <!-- Content -->
+      <!-- Content -->
         <div class="parallax-container">
           <div class="parallax"><img src="src/img/fallingJordan.jpg"></div>
         </div>
-        <div class="section white">
-          <div class="row container">
-            <h2 class="header teal-text">Le Premier site de la sneaker</h2>
-            <p class="grey-text text-darken-3 lighten-3">Sur ce site retrouvez toutes les dernieres sorties de vos marques préférées de baskets. </p>
-            <p class="grey-text text-darken-3 lighten-3"># Feel the Hype</p>
+        <div class="section container white">
+          <div class="row">
+            <h2 class="header center teal-text">Le Premier site de la sneaker</h2>
+            <p class="grey-text center text-darken-3 lighten-3">Sur ce site retrouvez toutes les dernieres sorties de vos marques préférées de baskets. </p>
+            <p class="grey-text center text-darken-3 lighten-3"># Feel the Hype</p>
           </div>
         </div>
         <div class="parallax-container">
           <div class="parallax"><img src="src/img/sneakerWall.jpg"></div>
         </div>
-        <!-- End Content -->
 
-        <!-- Footer -->
-        <footer class="page-footer teal">
-            <div class="container">
-            <div class="row">
-              <div class="col l6 s12">
-                <h5 class="white-text">Sneaker Shop</h5>
-                <p class="grey-text text-lighten-4">Le site préféré des sneaker Head</p>
+        <!-- last products -->
+        <div class="section container white">
+          <div class="row">
+            <h2 class="header teal-text">Découvrez les nouveaux modèles</h2>
+          </div>
+          <div class="row">
+            <div class=" col l4 m12 s12 card">
+              <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="src/img/products/jordan/jordan1BlueRoyal/1.jpg">
               </div>
-              <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Links</h5>
-                <ul>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-                </ul>
-              </div>
-            </div>
-            </div>
-            <div class="footer-copyright">
-            <div class="container">
-            Thibault Production
-            <a class="grey-text text-lighten-4 right" href="#!">site map</a>
-            </div>
-            </div>
-        </footer>
-        <!-- End Footer -->
+              <div class="card-content">
+                <span class="card-title activator grey-text text-darken-4">Air Jordan 1<i class="material-icons right">more_vert</i></span>
 
+                <p>Royal Blue / Black</p>
+              </div>
+
+              <div class="card-action">
+                <a href="#" class="teal-text">139,90 €</a>
+                <a href="#" class="teal-text"><i class="material-icons right">add_shopping_cart</i></a>
+              </div>
+
+              <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">Air Jordan 1<i class="material-icons right">close</i></span>
+                <p>
+                  <span>La jordan 1 dans son coloris Bleu royal.</span>
+                  <span>Réedition du modèle iconique de la marque Jordan dans son coloris bleu royal et noir</span>
+                </p>
+                <p><a href="#" class="teal-text">plus de détails</a></p>
+              </div>
+            </div>
+            <div class=" col l4 m12 s12 card">
+              <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="src/img/products/jordan/jordan1BlueRoyal/1.jpg">
+              </div>
+              <div class="card-content">
+                <span class="card-title activator grey-text text-darken-4">Air Jordan 1<i class="material-icons right">more_vert</i></span>
+
+                <p>Royal Blue / Black</p>
+              </div>
+
+              <div class="card-action">
+                <a href="#" class="teal-text">139,90 €</a>
+                <a href="#" class="teal-text"><i class="material-icons right">add_shopping_cart</i></a>
+              </div>
+
+              <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">Air Jordan 1<i class="material-icons right">close</i></span>
+                <p>
+                  <span>La jordan 1 dans son coloris Bleu royal.</span>
+                  <span>Réedition du modèle iconique de la marque Jordan dans son coloris bleu royal et noir</span>
+                </p>
+                <p><a href="#" class="teal-text">plus de détails</a></p>
+              </div>
+            </div>
+            <div class=" col l4 m12 s12 card">
+              <div class="card-image waves-effect waves-block waves-light">
+                <img class="activator" src="src/img/products/jordan/jordan1BlueRoyal/1.jpg">
+              </div>
+              <div class="card-content">
+                <span class="card-title activator grey-text text-darken-4">Air Jordan 1<i class="material-icons right">more_vert</i></span>
+
+                <p>Royal Blue / Black</p>
+              </div>
+
+              <div class="card-action">
+                <a href="#" class="teal-text">139,90 €</a>
+                <a href="#" class="teal-text"><i class="material-icons right">add_shopping_cart</i></a>
+              </div>
+
+              <div class="card-reveal">
+                <span class="card-title grey-text text-darken-4">Air Jordan 1<i class="material-icons right">close</i></span>
+                <p>
+                  <span>La jordan 1 dans son coloris Bleu royal.</span>
+                  <span>Réedition du modèle iconique de la marque Jordan dans son coloris bleu royal et noir</span>
+                </p>
+                <p><a href="#" class="teal-text">plus de détails</a></p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      <!-- End Content -->
+
+      <!-- Footer -->
+      <footer class="page-footer teal">
+          <div class="container">
+          <div class="row">
+            <div class="col l4 s12">
+              <h5 class="white-text">Sneaker Shop</h5>
+              <p class="grey-text text-lighten-4">Le site préféré des sneaker Head</p>
+            </div>
+            <div class="col l4 offset-l2 s12">
+              <h5 class="white-text">Links</h5>
+              <ul>
+                <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+              </ul>
+            </div>
+          </div>
+          </div>
+          <div class="footer-copyright">
+          <div class="container">
+          Thibault Production
+          <a class="grey-text text-lighten-4 right" href="#!">site map</a>
+          </div>
+          </div>
+      </footer>
+      <!-- End Footer -->
       </div>
+
       <!--Import jQuery before materialize.js-->
       <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
       <script type="text/javascript" src="js/materialize.min.js"></script>
