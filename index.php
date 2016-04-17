9b9baed827ac5d9f99a489c929971cf7cb27279e
ThibaultRic <mail@thibault-richard.com>
2016-04-17 15:01:05 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+        <title>Sneaker Shop</title> 
+
+        <!--Import Google Icon Font-->
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--Import materialize.css-->
+        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+        <link rel="stylesheet" type="text/css" href="css/main.css" media="screen,projection">
+
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+        <nav>
+            <div class="nav-wrapper teal">
+                <a href="index.php" class="brand-logo center">Sneaker Shop</a>
+                <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+                <ul class="left hide-on-med-and-down">
+                    <li><a href="#">produits</a></li>
+                    <li><a href="collapsible.html">Javascript</a></li>
+                    <li><a href="mobile.html">Mobile</a></li>
+                </ul>
+                <ul class="side-nav teal" id="mobile-demo">
+                    <li><a href="sass.html" class="white-text">Sass</a></li>
+                    <li><a href="badges.html" class="white-text">Components</a></li>
+                    <li><a href="collapsible.html" class="white-text">Javascript</a></li>
+                    <li><a href="mobile.html" class="white-text">Mobile</a></li>
+                </ul>
+                <a href="#" data-activates="mobile-demo"><i class="right material-icons">shopping_cart</i>
+            </div>
+        </nav>
+
+        <footer class="page-footer teal">
+            <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Sneaker Shop</h5>
+                <p class="grey-text text-lighten-4">Le site préféré des sneaker Head</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+            </div>
+            </div>
+            <div class="footer-copyright">
+            <div class="container">
+            Thibault Production
+            <a class="grey-text text-lighten-4 right" href="#!">site map</a>
+            </div>
+            </div>
+        </footer>
+      <!--Import jQuery before materialize.js-->
+      <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js"></script>
+      <script type="text/javascript" src="js/main.js"></script>
+    </body>
+  </html>
