f4691dbf43726f58503840b609325c3edc40e419
ThibaultRic <mail@thibault-richard.com>
2016-04-24 22:38:13 +0200

    connecting quantity to size Table

--- js/product.js
+++ js/product.js
@@ -1,6 +1,7 @@
 $(document)
     .ready(function()
     {
+        // init du slider
         $(".owl-carousel").owlCarousel({
             items:1,
             animateOut:'fadeOut',
@@ -9,35 +10,50 @@ $(document)
             startPosition: 'URLHash',
             autoHeight:true,
         });
-    });
 
-    $(function(){
-        $(".sizeTable a").click(function () {
-             $(this).addClass("selected").siblings().removeClass("selected");
-             $(this).removeClass('grey lighten-4').addClass("teal white-text").siblings().removeClass("teal white-text").addClass('grey lighten-4');
-            $('.shopBtn a').html('<i class="shoppingCart left material-icons">shopping_cart</i>Add to cart</a>');
+        // interaction au clic sur le tableau des tailles
+        $(function clickSize(){
+            $(".sizeTable a").click(function () {
+                if(!$(this).hasClass('disabled')){
+                    $(this).addClass("selected").siblings().removeClass("selected");
+                    // $(this).removeClass('grey lighten-4').addClass("teal white-text").siblings().removeClass("teal white-text");
+                    $('.shopBtn a').html('<i class="shoppingCart left material-icons">shopping_cart</i>Add to cart</a>');
+            }
 
+            });
         });
-    })
 
-    $(function() {
-        $(".shopBtn a").hover(function(){
-            if(!$(".sizeTable a").hasClass("selected"))
-            {
-                $('.shopBtn a').html('<i class="material-icons left">arrow_upward</i> Choose size first');
-                $('.shopBtn').addClass('nothingToCart');
+        // Disable size choice by quantity
+        $('a[data-qty]').each(function(){
+            if ($(this).attr('data-qty') == 0){
+                $(this).addClass('disableText disabled').removeClass('available');
             }
-        })
-    })
-
-    $(function() {
-        $(".shopBtn a").click(function(){
-            if(!$(".sizeTable a").hasClass("selected"))
-            {
-                $('.shopBtn a').html('<i class="material-icons left">arrow_upward</i> Choose size first');
-                $('.shopBtn').addClass('nothingToCart');
+            else{
+                $(this).addClass('available')
             }
-        })
-    })
+        });
 
+        // Interaction au hover sur bouton Add to cart Desktop
+        $(function() {
+            $(".shopBtn a").hover(function(){
+                if(!$(".sizeTable a").hasClass("selected"))
+                {
+                    $('.shopBtn a').html('<i class="material-icons left">arrow_upward</i> Choose size first');
+                    $('.shopBtn').addClass('nothingToCart');
+                }
+            })
+        });
+
+        // Interaction au click sur bouton Add to cart Mobile
+        $(function() {
+            $(".shopBtn a").click(function(){
+                if(!$(".sizeTable a").hasClass("selected"))
+                {
+                    $('.shopBtn a').html('<i class="material-icons left">arrow_upward</i> Choose size first');
+                    $('.shopBtn').addClass('nothingToCart');
+                }
+            });
+        });
+
+    });
 
--- moduls/_sizeTable.php
+++ moduls/_sizeTable.php
@@ -1,10 +1,13 @@
 <div class="sizeTable">
 <?php
-    $sizeQuery = mysql_query("SELECT * FROM sizeChart");
+    $sizeQuery = mysql_query("SELECT * FROM sizeChart s
+    LEFT JOIN product_has_sizeChart phs ON phs.sizeChart_id = s.id
+        WHERE phs.product_id = $daProduct");
     while ($row = mysql_fetch_array($sizeQuery, MYSQL_BOTH)) {
         $chart = $row['sizeChart'];
+        $qty = $row['quantity']
          ?>
-            <a class="sizeBtn waves-effect waves-light left center-align btn-flat grey lighten-4"><?php echo $chart ?>
+            <a data-qty="<?php echo $qty ?>" class="sizeBtn waves-effect waves-light left center-align btn-flat"><?php echo $chart ?>
          </a>
          <?php
         }
