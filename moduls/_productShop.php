20dc150acebf85294069a3172724c4d40047fd34
ThibaultRic <mail@thibault-richard.com>
2016-04-24 22:49:42 +0200

    last info added on product page

--- moduls/_productShop.php
+++ moduls/_productShop.php
@@ -1,12 +1,19 @@
 <div class="product-shop col s12 m4 l4 white " >
-    <h5><?php echo $brand ?></h5>
+    <h5 class="teal-text"><?php echo $brand ?></h5>
 
     <h1 class="header center"><?php echo $name ?></h1>
-    <h3 class="center teal-text"><?php echo $price?>€</h3>
+    <h3 class="center teal-text"><?php echo $price?> €</h3>
     <div class="size">
     <?php include '_sizeTable.php'; ?>
     </div>
+    <div class="row"></div>
+
     <div class="white center shopBtn">
         <a class="waves-effect waves-light btn-large"><i class="shoppingCart left material-icons">shopping_cart</i>Add to cart</a>
+    </div>
+    <div class="row"></div>
+    <div>
+    <p class="flow-text grey-text"><?php echo $description ?>
+    </p>
     </div>
 </div>
