3e9e7f3ea4e3e5a6e77ffddcb728c84e87938517
ThibaultRic <mail@thibault-richard.com>
2016-04-26 00:46:02 +0200

    add background white on body

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ include 'dbconnect.php';
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
 
-    <body>
+    <body class="white">
 
 
       <!-- Menu -->
--- product.php
+++ product.php
@@ -30,7 +30,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     </head>
 
-    <body>
+    <body class="white">
 
     <?php include 'moduls/_nav.php'; ?>
 
