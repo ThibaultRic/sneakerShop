fe10762dfde946076a6c58116b962fb67e0fbe7f
ThibaultRic <mail@thibault-richard.com>
2016-04-25 13:49:07 +0200

    Starting dynamic form

--- js/main.js
+++ js/main.js
@@ -7,6 +7,16 @@ $(document)
         $('select').material_select();
         $('.slider').slider({full_width: false});
         $('.materialboxed').materialbox();
+
+        function submitForm(){
+            var first_name = document.getElementById('first_name').value;
+            var last_name = document.getElementById('last_name').value;
+            var email = document.getElementById('email').value;
+            var reasons = document.getElementById('reasons').value;
+            var textarea1 = document.getElementById('textarea1').value;
+            var optinNewsletter = document.getElementById('optinNewsletter').value;
+            var dataString = 'name='+ first_name + '&last_name='+ last_name + '&email='+ email + '&email='+ email + '&reasons='+ reasons + '&textarea1='+ textarea1 + '&optinNewsletter='+ optinNewsletter;
+        }
     });
 
 
--- moduls/_productShop.php
+++ moduls/_productShop.php
@@ -1,7 +1,7 @@
 <div class="product-shop col s12 m4 l4 white " >
     <h5 class="teal-text"><?php echo $brand ?></h5>
 
-    <h1 class="header center"><?php echo $name ?></h1>
+    <h2 class="header center"><?php echo $name ?></h2>
     <h3 class="center teal-text"><?php echo $price?> €</h3>
     <div class="size">
     <?php include '_sizeTable.php'; ?>
@@ -13,7 +13,7 @@
     </div>
     <div class="row"></div>
     <div>
-    <p class="flow-text grey-text"><?php echo $description ?>
+    <p class="grey-text"><?php echo $description ?>
     </p>
     </div>
 </div>
